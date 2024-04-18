686581fd7c3bb7440c194d673a4c98252ca8545e
Himanshu-Tokie <himanshu.saha@chicmic.co.in>
2024-04-18 16:58:15 +0530

    animation

--- App.tsx
+++ App.tsx
@@ -6,6 +6,8 @@ import { useGetPokemonByNameQuery, useLazyGetPokemonByNameQuery } from "./src/rt
 import { FETCH_DATA } from "./src/action";
 import { fetchPokemon } from "./src/store";
 // import { fetchPokemon } from "./src/store";
+import Animated, { useAnimatedStyle, useSharedValue, withSpring } from 'react-native-reanimated';
+import { Circle, Svg } from 'react-native-svg';
 
 export default function App() {
   const dispatch = useDispatch();
@@ -28,22 +30,47 @@ const [getUsers, results] = useLazyGetPokemonByNameQuery('bulbasaur');
       dispatch(fetchPokemon(results.data));
     }
   };
+  const width = useSharedValue(10)
+
+  function handleWidth(){
+    translateX.value=withSpring(translateX.value+20)
+  }
+  const translateX = useSharedValue(0)
+  const animatedStyles = useAnimatedStyle(() => ({
+    transform: [{ translateX: withSpring(translateX.value * -2) }],
+  }));
+
+  const AnimatedCircle = Animated.createAnimatedComponent(Circle);
 
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView>
-      <View>
+      {/* <View>
         <Button title="Fetch Data" onPress={fetchData} />
         <Text>{JSON.stringify(selector.myReducer.saga)}</Text>
-      </View>
+      </View> */}
       {/* <View style={styles.imageContainer}>
         {Object.keys(selector).length !== 0 &&
         <Image source={{uri:selector.results[0].picture.large}} style={styles.image}></Image>}
       </View> */}
-      <View>
+      {/* <View>
         <Button title="Fetch Data Through Toolkit" onPress={fetchDataAndDispatch} />
         <Text>{JSON.stringify(selector.pokemonApi)}</Text>
-      </View>
+      </View> */}
+      <Animated.View
+      style={[styles.box,animatedStyles]}
+    />
+    <Button title="Width Handler" onPress={handleWidth}></Button>
+    <View style={styles.circleContainer}>
+    <Svg height="50%" width="50%" viewBox="0 0 100 100" >
+      <Circle cx="50" cy="50" r="10" fill="red" /></Svg>
+    </View>
+    <View style={styles.container}>
+      <Svg height="50%" width="50%" viewBox="0 0 100 100" >
+        <Circle cx="50" cy="50" r="50" stroke="purple" strokeWidth=".5" fill="violet" />
+      </Svg>
+    </View>
+    <Button title="Width Handler" onPress={handleWidth}></Button>
       </ScrollView>
     </SafeAreaView>
   );
@@ -52,6 +79,7 @@ const [getUsers, results] = useLazyGetPokemonByNameQuery('bulbasaur');
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    alignItems:'center'
   },
   image:{
     height:50,
@@ -61,7 +89,19 @@ const styles = StyleSheet.create({
     // alignContent:'center',
     alignItems:'center',
     // justifyContent:'center'
-  }
+  },
+  box:{
+    height: 120,
+    width: 120,
+    backgroundColor: '#b58df1',
+    borderRadius: 20,
+    marginVertical: 50,
+  },
+  circleContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 20, // Adjust the margin as needed
+  },
 });
 
 // import React from "react";
